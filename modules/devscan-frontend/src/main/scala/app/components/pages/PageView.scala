122c66803c1def6bc24844e389ab39a2c2c8310c
inverseplay <pure@inverseplay.com>
2023-05-14 16:56:42 +0900

    add resume model

--- modules/devscan-frontend/src/main/scala/app/components/pages/PageView.scala
+++ modules/devscan-frontend/src/main/scala/app/components/pages/PageView.scala
@@ -3,10 +3,12 @@ import tyrian.*
 import cats.effect.IO
 import tyrian.Html.*
 import Css.*
+import app.JsonData.resume
 
 object PageView:
 
   def view(model: Model): Html[Msg] =
     div(`class` := s"$page")(
-      "페이지 입니다"
+      p(`class` := "text-white")(model.current_depth)
+      // p(`class` := "text-white")(resume)
     )
