658799d16c4f8adea22a26d1fe7d3dcdd9e0d673
inverseplay <pure@inverseplay.com>
2023-05-21 21:54:17 +0900

    clean

--- modules/parseto-frontend/src/main/scala/app/controllers/view/View.scala
+++ modules/parseto-frontend/src/main/scala/app/controllers/view/View.scala
@@ -9,9 +9,6 @@ object View:
   def view(model: Model): Html[Msg] =
     div(`class` := s"${"bg-black h-screen w-screen"}")(
       {
-
-        // log2("parseJsonString1")(parseJsonString1)
-
         div(`class` := s"${"text-white"}")(
           "parseJsonString1.toString"
         )
