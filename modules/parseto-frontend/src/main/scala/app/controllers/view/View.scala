0f682ed6bf2f4a955ae3f23b2a0915764c20a340
inverseplay <pure@inverseplay.com>
2023-05-21 21:49:26 +0900

    fix

--- modules/parseto-frontend/src/main/scala/app/controllers/view/View.scala
+++ modules/parseto-frontend/src/main/scala/app/controllers/view/View.scala
@@ -3,19 +3,17 @@ import tyrian.*
 import cats.effect.IO
 
 import tyrian.Html.*
-import parseto.common.pipe.*
-import parseto.common.function.Log.*
-import parseto.common.pipe.Pipe.*
+import app.parseto.common._api.*
 
 object View:
   def view(model: Model): Html[Msg] =
     div(`class` := s"${"bg-black h-screen w-screen"}")(
       {
 
-        log2("parseJsonString1")(parseJsonString1)
+        // log2("parseJsonString1")(parseJsonString1)
 
         div(`class` := s"${"text-white"}")(
-          parseJsonString1.toString
+          "parseJsonString1.toString"
         )
       }
     )
