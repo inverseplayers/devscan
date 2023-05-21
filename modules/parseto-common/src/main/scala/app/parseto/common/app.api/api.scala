452d90204a19be09a30c1d93e5732dc48bea2f1b
inverseplay <pure@inverseplay.com>
2023-05-22 00:09:36 +0900

    add api

---
+++ modules/parseto-common/src/main/scala/app/parseto/common/app.api/api.scala
@@ -0,0 +1,15 @@
+package app.parseto.common._api
+// import app.parseto.common._api.*
+
+export app.parseto.common.parser.{
+  json2json_update,
+  string2json,
+  json2string_foldable,
+  getValue,
+  json2kv_list
+}
+export app.parseto.common.function.logs.{log, log2}
+export app.parseto.common.data.JsonString.*
+
+object API:
+  val a = "api..."
